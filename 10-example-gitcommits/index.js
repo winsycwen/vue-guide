747e8c2aa40cb2719bc22f521a3d3261e8c4d10b
winsycwen <winsycwen@gmail.com>
2017-02-27 23:12:13 +0800

    Added 根据vue官网示例进行练习

--- 10-example-gitcommits/index.js
+++ 10-example-gitcommits/index.js
@@ -1,4 +1,4 @@
-!(function(){
+/*!(function(){
 	function dealData(data) {
 		var len = data.length,
 			list = [],
@@ -6,6 +6,7 @@
 		if(len>0) {
 			for(var i = 0; i < len; i++) {
 				tempObject['time'] = data[i]['commit']['committer']['date'];
+				tempObject['msg'] = data[i]['commit']['message'];
 				list.push(tempObject);
 			}
 		}
@@ -22,7 +23,6 @@
 			filters: {
 				format: function(value) {
 					var date = value.split('T')[0];
-					console.log(date);
 					return date;
 				}
 			}
@@ -43,4 +43,50 @@
 		};
 	}
 	getData(dealData);
+})();*/
+
+!(function() {
+	var demo = new Vue({
+		el: '#demo',
+		data: {
+			commits: []
+		},
+		created: function() {
+			this.getData();
+		},
+		methods: {
+			getData: function() {
+				var apiURL = 'https://api.github.com/repos/winsycwen/winsycwen.github.io/commits';
+				var xhr = new XMLHttpRequest();
+				var self = this;
+				xhr.open('get', apiURL, true);
+				xhr.send();
+				xhr.onreadystatechange = function() {
+					if(xhr.readyState == 4) {
+						if(xhr.status >= 200 && xhr.status <300) {
+							self.dealData(JSON.parse(xhr.responseText));
+						}
+					}
+				}
+			},
+			dealData: function(data) {
+				var len = data.length,
+					list = [],
+					tempObject = {};
+				if(len>0) {
+					for(var i = 0; i < len; i++) {
+						tempObject['time'] = data[i]['commit']['committer']['date'];
+						tempObject['msg'] = data[i]['commit']['message'];
+						list.push(tempObject);
+					}
+				}
+				this.commits = list;
+			}
+		},
+		filters: {
+			format: function(value) {
+				return value.split('T')[0];
+			}
+		}
+	});
 })();
