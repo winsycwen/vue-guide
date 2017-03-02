68a90cd605150e153b35308df3c8fbb81c12b3e6
winsycwen <winsycwen@gmail.com>
2017-03-02 22:07:25 +0800

    Updated 修改gitcommit示例代码

--- 10-example-gitcommits/index.js
+++ 10-example-gitcommits/index.js
@@ -1,66 +1,23 @@
-/*!(function(){
-	function dealData(data) {
-		var len = data.length,
-			list = [],
-			tempObject = {};
-		if(len>0) {
-			for(var i = 0; i < len; i++) {
-				tempObject['time'] = data[i]['commit']['committer']['date'];
-				tempObject['msg'] = data[i]['commit']['message'];
-				list.push(tempObject);
-			}
-		}
-		
-		render(list);
-	}
-	
-	function render(list) {
-		new Vue({
-			el: '.commit-wrapper',
-			data: {
-				commits: list
-			},
-			filters: {
-				format: function(value) {
-					var date = value.split('T')[0];
-					return date;
-				}
-			}
-		});
-	}
-	
-	function getData(callback) {
-		var xhr = new XMLHttpRequest();
-		var url =		 'https://api.github.com/repos/winsycwen/winsycwen.github.io/commits';
-		xhr.open("get", url, true);
-		xhr.send();
-		xhr.onreadystatechange = function() {
-			if(xhr.readyState == 4) {
-				if(xhr.status >= 200 && xhr.status < 300) {
-					callback && callback(JSON.parse(xhr.responseText));
-				}
-			}
-		};
-	}
-	getData(dealData);
-})();*/
-
 !(function() {
 	var demo = new Vue({
 		el: '#demo',
 		data: {
-			commits: []
+			commits: [],
+			branches: ['master', 'gh-pages'],
+			currentBranch: 'gh-pages'
+		},
+		watch: {
+			currentBranch: 'getData'
 		},
 		created: function() {
 			this.getData();
 		},
 		methods: {
 			getData: function() {
-				var apiURL = 'https://api.github.com/repos/winsycwen/winsycwen.github.io/commits';
+				var apiURL = 'https://api.github.com/repos/winsycwen/winsycwen.github.io/commits?sha=';
 				var xhr = new XMLHttpRequest();
 				var self = this;
-				xhr.open('get', apiURL, true);
-				xhr.send();
+				xhr.open('get', apiURL + self.currentBranch, true);
 				xhr.onreadystatechange = function() {
 					if(xhr.readyState == 4) {
 						if(xhr.status >= 200 && xhr.status <300) {
@@ -68,6 +25,7 @@
 						}
 					}
 				}
+				xhr.send(null);
 			},
 			dealData: function(data) {
 				var len = data.length,
@@ -75,6 +33,8 @@
 					tempObject = {};
 				if(len>0) {
 					for(var i = 0; i < len; i++) {
+						tempObject = {};
+						tempObject['sha'] = data[i]['sha'];
 						tempObject['time'] = data[i]['commit']['committer']['date'];
 						tempObject['msg'] = data[i]['commit']['message'];
 						list.push(tempObject);
